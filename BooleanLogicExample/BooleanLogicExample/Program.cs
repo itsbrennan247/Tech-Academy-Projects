2e8b0b58f41878bcc0cd4fcb461c2e3072241751
itsbrennan247 <37353955+itsbrennan247@users.noreply.github.com>
2018-07-18 13:59:03 -0700

    Add files via upload

---
+++ BooleanLogicExample/BooleanLogicExample/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanLogicExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            Console.WriteLine("What is your age?");
+            age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Have you ever had a DUI? Please write TRUE or FALSE.");
+            String dui = Console.ReadLine();
+           
+            
+            int tickets;
+            Console.WriteLine("How many speeding tickets do you have?");
+            tickets = Convert.ToInt32(Console.ReadLine());
+
+            if ((age > 15) && (tickets < 3) && (dui == "false"))
+            {
+                Console.WriteLine("Congrats you qualify!");
+            }
+            else
+            {
+                Console.WriteLine("Sorry you do not qualify!");
+            }
+            Console.ReadLine();
+        }
+    }
+}
