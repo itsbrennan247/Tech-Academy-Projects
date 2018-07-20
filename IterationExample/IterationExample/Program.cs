65fe7d34fde0d0aac0f30393cee029cd44127591
itsbrennan247 <37353955+itsbrennan247@users.noreply.github.com>
2018-07-19 20:17:27 -0700

    Add files via upload

---
+++ IterationExample/IterationExample/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] teams = { "1", "2", "3", "4", "5" };
+
+        Console.WriteLine("Please enter in 5 of your favorite sports teams!");
+
+        teams[0] = Console.ReadLine();
+        teams[1] = Console.ReadLine();
+        teams[2] = Console.ReadLine();
+        teams[3] = Console.ReadLine();
+        teams[4] = Console.ReadLine();
+
+        Console.WriteLine("Here's a list of your favorite teams");
+        
+        foreach (string team in teams)
+        {
+            Console.WriteLine(team);
+        }
+        Console.ReadLine();
+
+        //Loop involving < operator
+        for (int i = 0; i < teams.Length; i++)
+        {
+            Console.WriteLine(teams[i]);
+        }
+        Console.ReadLine();
+
+        //Loop involving <= operator
+        for (int i = 0; i <= teams.Length; i++)
+        {
+            Console.WriteLine(teams[i]);
+            return;
+        }
+        Console.ReadLine();
+
+    }
+}
